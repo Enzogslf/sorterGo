bb1d558034f5d198f7396b054f691d82fb85a2ef
Enzogslf <cyudef@gmail.com>
2023-05-29 23:19:05 -0300

    just a commit commitment
    :)

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// checking if everything is working as usual :) its 23:00 so ill get back to it maybe in the morning? Commit
+func main() {
+	fmt.Println("hello!")
+}
