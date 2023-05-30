8071cdd316f93b730c910b7ccda81f99ed557e6b
Enzogslf <cyudef@gmail.com>
2023-05-30 18:17:35 -0300

    ill get back to it in some time
    ill update when i learn algorithms better

--- main.go
+++ main.go
@@ -2,9 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-// checking if everything is working as usual :) its 23:00 so ill get back to it maybe in the morning? Commit
+// ngl i copied this off of youtube, still trying to understand the code the guy in the tutorial didnt rly explain anything
 func main() {
-	fmt.Println("hello!")
+	//lenght of array to be sorted
+	piece := createpiece(15)
+	fmt.Println("\n Unsorted: \n", piece)
+	quicksort(piece)
+	fmt.Println("\n sorted \n", piece)
+}
+
+// makes array with numbers from 999 to -999
+func createpiece(size int) []int {
+	piece := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		piece[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return piece
+}
+func quicksort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+	left, right := 0, len(a)-1
+	center := rand.Int() % len(a)
+	a[center], a[right] = a[right], a[center]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+	a[left], a[right] = a[right], a[left]
+	quicksort(a[:left])
+	quicksort(a[left+1:])
+	return a
 }
